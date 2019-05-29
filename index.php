bf512673263dada29c672c4527b3630fb8d00736
Andreas Halvorsen Tollånes <andreas@tollanes.com>
2019-05-29 02:43:07 +0200

    added basic containers

--- index.php
+++ index.php
@@ -2,5 +2,36 @@
 
 <?php startblock('content') ?>
 
+<section id="header-index" class="one-page">
+
+</section>
+
+<section class="container-normal">
+
+</section>
+
+<section id="hero01" class="one-page">
+
+</section>
+
+<section  class="container-normal">
+
+</section>
+
+<section id="hero02" class="one-page">
+
+</section>
+
+<section  class="container-normal">
+
+</section>
+
+<section id="hero03" class="one-page">
+
+</section>
+
+<section  class="container-normal">
+
+</section>
 
 <?php endblock() ?>
--- layout.php
+++ layout.php
@@ -10,8 +10,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="keywords" content="<?php print $KEYWORDS;?>" />
         <meta name="description" content="<?php print $DESCRIPTION;?>" />
-<!--        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">-->
-        <link href="https://fonts.googleapis.com/css?family=Lato|Parisienne" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.typekit.net/bfn8hng.css">
+
         <link rel="stylesheet" href="css/base.css">
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
@@ -20,17 +20,22 @@
         <?php endblock() ?>
 
     </head>
+
     <body>
+        <div class="bg-main"></div>
 
 <!--        --><?php //if ($CURRENT_PAGE == "Index") {include("includes/index_header.php"); } else {include("includes/header.php");};?>
         <?php include("includes/header.php"); ?>
-        <?php startblock('content') ?>
 
-        <?php endblock() ?>
+        <div class="container-main">
+            <?php startblock('content') ?>
 
-        <footer>
+            <?php endblock() ?>
+        </div>
 
-        </footer>
+<!--        <footer>-->
+<!---->
+<!--        </footer>-->
 
         <script type="text/javascript" src="/js/main.js"></script>
         <?php startblock('scripts') ?>
